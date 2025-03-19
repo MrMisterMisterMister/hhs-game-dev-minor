3b722a67f49a107cc348c6bfe54b3d4c064200ac
Sly <114733626+MrMisterMisterMister@users.noreply.github.com>
2025-03-19 23:24:01 +0100

    Remove gold 1

--- gold-1-finite-state-machine/ui/ui.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-@onready var label: Label = $Label
-
-
-func _on_state_machine_state_changed(current_state: State) -> void:
-	if label != null:
-		label.text = "Current State: " + current_state.name
